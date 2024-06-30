728e0ce4ef733721edfe4ba404b7b9cf8de30f35
Lucas Tonelotto <lucasfrantone@gmail.com>
2024-06-30 12:14:54 -0300

    chore: Update npm dependency for cookie-parser to version 1.4.6

--- index.js
+++ index.js
@@ -2,15 +2,28 @@ import express from 'express'
 import { PORT, SECRET_KEY } from './config.js'
 import { UserRepository } from './user-repository.js'
 import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
 app.set('view engine', 'ejs')
 
 app.use(express.json())
+app.use(cookieParser())
+app.use((req, res, next) => {
+  const token = req.cookies.acces_token
+  req.session = { user: null }
+
+  try {
+    const data = jwt.verify(token, SECRET_KEY)
+    req.session.user = data
+  } catch {}
+  next()
+})
 
 app.get('/', (req, res) => {
-  res.render('index')
+  const { user } = req.session
+  res.render('index', user)
 })
 
 app.post('/login', async (req, res) => {
@@ -20,7 +33,14 @@ app.post('/login', async (req, res) => {
   try {
     const user = await UserRepository.login({ username, password })
     const token = jwt.sign({ id: user._id, username: user.username }, SECRET_KEY, { expiresIn: '1h' })
-    res.send({ user })
+    res
+      .cookie('acces_token', token, {
+        httpOnly: true,
+        sameSite: 'strict',
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 3600000
+      })
+      .send({ user, token })
   } catch (error) {
     res.status(401).send({ error: error.message })
   }
@@ -38,10 +58,16 @@ app.post('/register', async (req, res) => {
   }
 })
 
-app.post('/logout', (req, res) => {})
+app.post('/logout', (req, res) => {
+  res
+    .clearCookie('acces_token')
+    .json({ message: 'Logged out' })
+})
 
 app.get('/protected', (req, res) => {
-  res.render('protected', { username: 'lutogan' })
+  const { user } = req.session
+  if (!user) return res.status(401).send({ error: 'Unauthorized' })
+  res.render('protected', user)
 })
 
 app.listen(PORT, () => {
